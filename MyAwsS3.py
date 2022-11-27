2e900ed9314dae57e864f0cf3ba7be0b45896548
imsehwan <dla612@naver.com>
2022-11-27 15:48:17 +0900

    feat : queryString으로 파일 입력받아서 s3에 저장하기 완료

--- MyAwsS3.py
+++ MyAwsS3.py
@@ -3,6 +3,7 @@ import boto3
 from botocore.exceptions import ClientError
 import os
 
+
 def upload_file(file_name, bucket, object_name=None):
     """Upload a file to an S3 bucket
 
@@ -18,7 +19,9 @@ def upload_file(file_name, bucket, object_name=None):
 
     # Upload the file
     s3_client = boto3.client('s3')
+
     try:
+        # ( 내가 업로드 할 파일, 업로드 될 버킷이름 , 해당 파일이 가질 이름
         response = s3_client.upload_file(file_name, bucket, object_name)
     except ClientError as e:
         logging.error(e)
--- main.py
+++ main.py
@@ -1,26 +1,57 @@
-from fastapi import FastAPI, File, UploadFile
+import datetime
+import os
+
+from fastapi import FastAPI, File, UploadFile, Path, Query
 import MyAwsS3
 from pydantic import BaseModel
+import logging
+from typing import Union
 
 app = FastAPI()
 
-bucket_name = 'makeanything'
+bucket_name = 'eagleeyes'
+
 
 class Item(BaseModel):
-    filename : str
+    filename: str
+
 
 @app.get("/")
 def read_root():
-  return { "Hello": "World" }
+    return {"Hello": "World"}
+
 
 @app.get("/fastapi-test")
 def test_api():
     return "hi"
 
+
 @app.post("/fastapi-test2")
-def test_api(example:str):
+def test_api(example: str):
     return example
 
+
 @app.post("/upload-file")
-def upload_file(item : Item):
+def upload_file(item: Item):
+    MyAwsS3.upload_file(item.filename, bucket_name, "listen")
+    return "okay"
+
+
+@app.post("/upload-fileV2")
+def upload_file(item: Item, file: UploadFile):
+    hi = dummy_function(item.filename)
     MyAwsS3.upload_file(item.filename, bucket_name)
+    os.remove(hi)
+    return "okay"
+
+
+@app.post("/upload-fileV3")
+def upload_file(
+        filename: str = Path
+):
+    MyAwsS3.upload_file(filename, bucket_name, "test.jpg")
+    return "okay"
+
+
+def dummy_function(item: Item):
+    return "hi"
