3b59e4c42f0c12a4161b48a79a052a77a18ef180
imsehwan <dla612@naver.com>
2022-11-28 16:56:45 +0900

    feat : 영상 처리 로직 추가

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ from pydantic import BaseModel
 import logging
 from typing import Union
 
+import desmoke
+
 app = FastAPI()
 
 bucket_name = 'eagleeyes'
+resultVideo_url = "/home/ubuntu/result/result.mp4"
 
 
 class Item(BaseModel):
@@ -44,10 +47,13 @@ def upload_file(
         file: str = Path,
         file1: str = Path
 ):
-    MyAwsS3.upload_file(filename, bucket_name, file)
+    # ( 연기를 제거하려는 원본영상의 경로, 최종 결과물이 저장되는 경로, 파일 이름(확장자포함) )
+    # 원본영상은 java 서버에서 제거 , 영상처리 후 결과물은 여기서 제거해야 함.
+    # desmoke.desmoke_video(filename, resultVideo_url, "result.mp4")
+    MyAwsS3.upload_file(resultVideo_url, bucket_name, file)
+
+    if os.path.exists(resultVideo_url)
+        os.remove(resultVideo_url)
+
     MyAwsS3.upload_file(filename1, bucket_name, file1)
     return "okay"
-
-
-def dummy_function(item: Item):
-    return "hi"
